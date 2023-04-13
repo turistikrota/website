fa35e93cc32afdf62aa4f1b29b3deb2ffe560eac
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-04-14 01:22:21 +0300

    project: add google analytics

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,21 +1,36 @@
-import { i18n } from '~/i18n-config'
-import "~/app/globals.css"
-import "boxicons/css/boxicons.min.css"
+import { i18n } from "~/i18n-config";
+import "~/app/globals.css";
+import "boxicons/css/boxicons.min.css";
+import Script from "next/script";
 
 export async function generateStaticParams() {
-    return i18n.locales.map((locale) => ({ lang: locale }))
+  return i18n.locales.map((locale) => ({ lang: locale }));
 }
 
 export default function Root({
-                                 children,
-                                 params,
-                             }: {
-    children: React.ReactNode
-    params: { lang: string }
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { lang: string };
 }) {
-    return (
-        <html lang={params.lang}>
-        <body>{children}</body>
-        </html>
-    )
+  return (
+    <html lang={params.lang}>
+      <body>
+        {children}
+        <Script
+          async={true}
+          src="https://www.googletagmanager.com/gtag/js?id=G-LX3MT1E36B"
+        ></Script>
+        <Script id="google-analytics-config" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-LX3MT1E36B');
+          `}
+        </Script>
+      </body>
+    </html>
+  );
 }
