8a981c89d5dee441ec359ac095816c0c8f5a0a7b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-05-05 16:59:36 +0300

    feat(#1): complete the our vision section

--- src/app/[locale]/about-us/components/MissionVisionSection.tsx
+++
@@ -1,5 +0,0 @@
-import { BaseProps } from "~/types/base";
-
-export default function MissionVisionSection({children}: BaseProps) {
-    return <section className="container my-24 px-6 mx-auto flex lg:flex-row flex-col md:gap-10 gap-16 justify-evenly">{children}</section>
-}
--- src/app/[locale]/about-us/page.tsx
+++ src/app/[locale]/about-us/page.tsx
@@ -1,7 +1,5 @@
 import HeadSection from "./components/HeadSection"
-import MissionVisionSection from "./components/MissionVisionSection"
 import OurFeatureSection from "./components/OurFeatureSection"
-import OurMissionSection from "./components/OurMissionSection"
 import OurTeamSection from "./components/OurTeamSection"
 import OurVisionSection from "./components/OurVisionSection"
 import TimelineSection from "./components/TimelineSection"
@@ -11,10 +9,7 @@ export default function AboutUs() {
         <HeadSection></HeadSection>
         <OurFeatureSection></OurFeatureSection>
         <OurTeamSection></OurTeamSection>
-            <TimelineSection></TimelineSection>
-            <MissionVisionSection>
-                <OurMissionSection></OurMissionSection>
-                <OurVisionSection></OurVisionSection>
-                </MissionVisionSection>
+        <TimelineSection></TimelineSection>
+        <OurVisionSection></OurVisionSection>
     </>
 }
