7c45f34965faa7ce94c4d3c78d5b37ed8d59d106
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-04 22:59:01 +0300

    update next config runtime

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
 import { useLocale } from "next-intl";
-import { redirect } from 'next-intl/server';
+import { redirect } from "next-intl/server";
 import { Page, staticRoute } from "~/static/pages";
 
 export default async function Home() {
-    const locale = useLocale();
-    redirect(staticRoute(Page.ComingSoon, locale))
+  const locale = useLocale();
+  redirect(staticRoute(Page.ComingSoon, locale));
 }
