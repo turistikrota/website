9003dfa5f3b2116aff2f56e945fdc0f9e0a59d58
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-10 23:40:40 +0300

    feat(auth): complete auth selection card and create card

--- src/features/account/AccountSelection.tsx
+++ src/features/account/AccountSelection.tsx
@@ -1,109 +1,12 @@
 "use client";
-import { useLocale, useTranslations } from "next-intl";
-import Image from "next/image";
-import { useState } from "react";
+import { useTranslations } from "next-intl";
 import { Condition } from "~/components";
-import { Config } from "~/config";
-import { useDayJS } from "~/utils/dayjs";
+import AccountSelectionCard from "./AccountSelectionCard";
 import { useListQuery } from "./account.api";
-import { AccountListItem, isAccountListResponse } from "./account.types";
+import { isAccountListResponse } from "./account.types";
 
 // avatar files like avatar_1.png, avatar_2.png, avatar_3.png
 // const avatarFiles = Array.from({ length: 24 }, (_, i) => `avatar_${i + 1}.png`);
-
-type AccountSelectionProps = {
-  onSelect: (name: string, code: string) => void;
-} & AccountListItem;
-
-const CompletedRangeLine = ({ completedRate }: { completedRate: number }) => {
-  return (
-    <div className="absolute bottom-0 left-0 w-full h-1 bg-green-50 dark:bg-green-950 bg-opacity-50 rounded-b-md">
-      <div
-        className={`h-full bg-green-400 dark:bg-green-500 rounded-b-md`}
-        style={{ width: `${completedRate}%` }}
-      ></div>
-    </div>
-  );
-};
-
-const VerifiedBadge = () => {
-  const t = useTranslations("general");
-  return (
-    <div
-      className="absolute top-0 right-0 flex items-center justify-center w-6 h-6 rounded-full"
-      role="alert"
-      aria-label={t("verified")}
-      title={t("verified_alt")}
-    >
-      <i className="bx bx-md bxs-badge-check text-primary-500"></i>
-    </div>
-  );
-};
-
-const AccountSelectionCard: React.FC<AccountSelectionProps> = ({
-  avatarUrl,
-  userName,
-  userCode,
-  completedRate,
-  createdAt,
-  description,
-  fullName,
-  isActive,
-  isVerified,
-  onSelect,
-}) => {
-  const t = useTranslations("general");
-  const locale = useLocale();
-  const [avatar, setAvatar] = useState<string>(avatarUrl);
-  const dayjs = useDayJS(locale);
-  return (
-    <div
-      className="grid grid-cols-3 relative cursor-pointer"
-      onClick={() => onSelect(userName, userCode)}
-    >
-      <div className="col-span-1 bg-default flex items-center justify-center p-4 rounded-tl-md rounded-bl-md">
-        <Image
-          src={avatar}
-          alt={userName}
-          width={100}
-          height={100}
-          onError={() => setAvatar(Config.cdn.notFound)}
-        />
-      </div>
-      <div className="col-span-2 bg-header flex flex-col p-4 rounded-tr-md rounded-br-md">
-        <div className="text-xl font-semibold text-gray-900 dark:text-white">
-          {userName}
-          <span className="text-sm text-gray-500"> #{userCode}</span>
-        </div>
-        <div className="text-sm text-gray-500 line-clamp-1">{fullName}</div>
-        <div className="text-sm text-gray-500 line-clamp-1">
-          {description} {description} {description} {description}
-          {description}
-        </div>
-        <div className="flex items-center justify-between mt-4">
-          <div className="text-sm text-gray-500">
-            {dayjs(createdAt).format("DD MMMM YYYY, HH:mm")}
-          </div>
-          <div
-            className={`text-sm ${
-              isActive ? "text-green-500" : "text-red-500"
-            }`}
-            role="alert"
-            aria-label={t(isActive ? "active" : "inactive")}
-            title={t(isActive ? "active" : "inactive")}
-          >
-            {t(isActive ? "active" : "inactive")}
-          </div>
-        </div>
-      </div>
-      <CompletedRangeLine completedRate={completedRate} />
-      <Condition value={isVerified}>
-        <VerifiedBadge />
-      </Condition>
-    </div>
-  );
-};
-
 export default function AccountSelection() {
   const t = useTranslations("account.select");
   const { isLoading, data, error } = useListQuery({});
@@ -116,8 +19,40 @@ export default function AccountSelection() {
         <div className="flex flex-col space-y-4">
           {isAccountListResponse(data) &&
             data.map((item, index) => (
-              <AccountSelectionCard key={index} {...item} onSelect={() => {}} />
+              <AccountSelectionCard key={index} {...item} onSelect={() => {}}>
+                <AccountSelectionCard.Image>
+                  <AccountSelectionCard.Avatar
+                    src={item.avatarUrl}
+                    alt={item.fullName}
+                  ></AccountSelectionCard.Avatar>
+                </AccountSelectionCard.Image>
+                <AccountSelectionCard.Content>
+                  <AccountSelectionCard.UserName>
+                    {item.userName}
+                    <AccountSelectionCard.UserCode>
+                      {item.userCode}
+                    </AccountSelectionCard.UserCode>
+                  </AccountSelectionCard.UserName>
+                  <AccountSelectionCard.FullName>
+                    {item.fullName}
+                  </AccountSelectionCard.FullName>
+                </AccountSelectionCard.Content>
+                <AccountSelectionCard.CompletedRangeLine
+                  completedRate={item.completedRate}
+                />
+                <Condition value={item.isVerified}>
+                  <AccountSelectionCard.VerifiedBadge />
+                </Condition>
+              </AccountSelectionCard>
             ))}
+          <AccountSelectionCard onSelect={() => {}}>
+            <AccountSelectionCard.Image>
+              <i className="bx bx-md bx-plus text-gray-400"></i>
+            </AccountSelectionCard.Image>
+            <AccountSelectionCard.Content>
+              <AccountSelectionCard.Text>{t("new")}</AccountSelectionCard.Text>
+            </AccountSelectionCard.Content>
+          </AccountSelectionCard>
         </div>
       </div>
     </div>
---
+++ src/features/account/AccountSelectionCard.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import { PropsWithChildren, useState } from "react";
+import { Config } from "~/config";
+
+type AccountSelectionProps = {
+  onSelect: () => void;
+};
+
+type AccountSelectionCardType = React.FC<
+  PropsWithChildren<AccountSelectionProps>
+> & {
+  CompletedRangeLine: typeof CompletedRangeLine;
+  VerifiedBadge: typeof VerifiedBadge;
+  Image: typeof AImage;
+  Content: typeof Content;
+  UserName: typeof UserName;
+  UserCode: typeof UserCode;
+  FullName: typeof FullName;
+  Avatar: typeof Avatar;
+  Text: typeof Text;
+};
+
+const CompletedRangeLine = ({ completedRate }: { completedRate: number }) => {
+  const t = useTranslations("account.select.tool");
+  return (
+    <div
+      className="absolute bottom-0 left-0 w-full h-1 bg-green-50 dark:bg-green-950 bg-opacity-50 rounded-b-md"
+      role="progressbar"
+      aria-valuenow={completedRate}
+      aria-valuemin={0}
+      aria-valuemax={100}
+      aria-label={t("completedRate", { rate: completedRate })}
+      title={t("completedRate", { rate: completedRate })}
+    >
+      <div
+        className={`h-full bg-green-400 dark:bg-green-500 rounded-b-md`}
+        style={{ width: `${completedRate}%` }}
+      ></div>
+    </div>
+  );
+};
+
+const VerifiedBadge = () => {
+  const t = useTranslations("general");
+  return (
+    <div
+      className="absolute top-0 right-0 flex items-center justify-center w-6 h-6 rounded-full"
+      role="alert"
+      aria-label={t("verified")}
+      title={t("verified_alt")}
+    >
+      <i className="bx bx-md bxs-badge-check text-primary-500"></i>
+    </div>
+  );
+};
+
+const AImage = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="col-span-1 bg-default flex items-center justify-center p-4 rounded-tl-md rounded-bl-md">
+      {children}
+    </div>
+  );
+};
+
+type AvatarProps = {
+  src: string;
+  alt: string;
+};
+
+const Avatar = ({ src, alt }: AvatarProps) => {
+  const [avatar, setAvatar] = useState<string>(src);
+  return (
+    <Image
+      src={avatar}
+      alt={alt}
+      width={70}
+      height={70}
+      onError={() => setAvatar(Config.cdn.notFound)}
+    />
+  );
+};
+
+const Content = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="col-span-3 bg-header flex flex-col justify-center p-4 rounded-tr-md rounded-br-md">
+      {children}
+    </div>
+  );
+};
+
+const UserName = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="text-xl font-semibold text-gray-900 dark:text-white">
+      {children}
+    </div>
+  );
+};
+
+const Text = ({ children }: React.PropsWithChildren) => {
+  return (
+    <span className="text-md font-semibold text-gray-900 dark:text-white">
+      {children}
+    </span>
+  );
+};
+
+const UserCode = ({ children }: React.PropsWithChildren) => {
+  return <span className="text-sm text-gray-500"> #{children}</span>;
+};
+
+const FullName = ({ children }: React.PropsWithChildren) => {
+  return <div className="text-sm text-gray-500 line-clamp-1">{children}</div>;
+};
+
+const Card: AccountSelectionCardType = ({ children, onSelect }) => {
+  return (
+    <div
+      className="grid grid-cols-4 relative cursor-pointer"
+      onClick={onSelect}
+    >
+      {children}
+    </div>
+  );
+};
+
+Card.CompletedRangeLine = CompletedRangeLine;
+Card.VerifiedBadge = VerifiedBadge;
+Card.Image = AImage;
+Card.Content = Content;
+Card.UserName = UserName;
+Card.UserCode = UserCode;
+Card.FullName = FullName;
+Card.Avatar = Avatar;
+Card.Text = Text;
+
+export default Card;
