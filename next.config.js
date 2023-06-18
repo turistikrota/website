3a3ee84c939fb82d4e2f3b8d4808900ecc21c892
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-19 00:06:50 +0300

    config: update next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const withMDX = require("@next/mdx")({
 });
 const withPwa = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
