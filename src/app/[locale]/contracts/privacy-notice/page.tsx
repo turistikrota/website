6b715f27b2cb27e37737ad99fb303bf9f2104baf
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-19 22:35:43 +0300

    feat(#45): add privacy notify page seo

--- src/app/[locale]/contracts/privacy-notice/page.tsx
+++ src/app/[locale]/contracts/privacy-notice/page.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from "next";
+import { getLocale, getTranslations } from "next-intl/server";
+import { getStaticRoute } from "~/static/page";
+import { generateDefaultMetadata } from "~/utils/meta";
 import PrivacyNoticeContent from "./content/Content";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations("contracts.privacy-notification");
+  const locale = getLocale();
+  return generateDefaultMetadata(locale, {
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords"),
+    page: {
+      tr: getStaticRoute("tr").contracts.privacyNotify,
+      en: getStaticRoute("en").contracts.privacyNotify,
+    },
+  });
+}
 
 export default function PrivacyNoticePage() {
   return <PrivacyNoticeContent />;
