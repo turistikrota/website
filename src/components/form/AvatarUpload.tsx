268f20f3859632cca8561dff7b33254d8e03d9a7
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-25 01:00:20 +0300

    feat(#53): complete upload avatar component for first time

--- src/components/form/AvatarUpload.tsx
+++ src/components/form/AvatarUpload.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { default as NextImage } from "next/image";
 import { useState } from "react";
 
 type Props = {
@@ -8,6 +9,10 @@ type Props = {
   onChange: (file: File) => void;
   minSize?: number;
   maxSize?: number;
+  minWidth?: number;
+  maxWidth?: number;
+  minHeight?: number;
+  maxHeight?: number;
   accept?: string;
 };
 
@@ -16,52 +21,91 @@ export default function AvatarUpload({
   onChange,
   minSize = 0,
   maxSize = 1,
+  minWidth = 500,
+  maxWidth = 500,
+  minHeight = 500,
+  maxHeight = 500,
   accept = "image/png",
 }: Props) {
   const [src, setSrc] = useState(avatar);
   const [error, setError] = useState<string | null>(null);
+  const t = useTranslations("avatar-form");
 
-  const validate = (file: File) => {
-    if (file.size < minSize * 1024) {
-      setError(`Dosya boyutu en az ${minSize} KB olmalıdır.`);
-      return false;
-    }
+  const validate = (file: File | undefined): Promise<File> => {
+    return new Promise((resolve, reject) => {
+      if (!file) return reject(t("errors.required").toString());
+      if (file.size < minSize * 1024 * 1024)
+        return reject(
+          t("errors.min", {
+            min: minSize,
+          }).toString()
+        );
 
-    if (file.size > maxSize * 1024) {
-      setError(`Dosya boyutu en fazla ${maxSize} KB olmalıdır.`);
-      return false;
-    }
+      if (file.size > maxSize * 1024 * 1024)
+        return reject(
+          t("errors.max", {
+            max: maxSize,
+          }).toString()
+        );
 
-    if (!file.type.startsWith(accept)) {
-      setError(`Dosya türü ${accept} olmalıdır.`);
-      return false;
-    }
+      if (!file.type.startsWith(accept))
+        return reject(t("errors.type", { accept }).toString());
 
-    setError(null);
-    return true;
+      const img = new Image();
+      img.src = URL.createObjectURL(file);
+      img.onload = () => {
+        if (img.width < minWidth || img.height < minHeight)
+          return reject(
+            t("errors.minSize", { minWidth, minHeight: minHeight }).toString()
+          );
+        if (img.width > maxWidth || img.height > maxHeight)
+          return reject(
+            t("errors.maxSize", { maxWidth, maxHeight: maxHeight }).toString()
+          );
+
+        return resolve(file);
+      };
+    });
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    if (file && validate(file)) {
-      setSrc(URL.createObjectURL(file));
-      onChange(file);
+    const validFile = await validate(file).catch((err) => {
+      setError(err);
+      return null;
+    });
+    if (validFile) {
+      setError(null);
+      setSrc(URL.createObjectURL(validFile));
+      onChange(validFile);
     }
+  };
+
+  const focus = () => {
+    document.getElementById("avatar")?.click();
   };
 
   return (
     <div className="flex flex-col items-center">
       <div className="relative">
-        <Image
+        <NextImage
           src={src}
           alt="Avatar"
+          title={t("upload")}
           width={150}
           height={150}
           className="object-cover w-52 h-52 rounded-full cursor-pointer bg-second border"
-          onClick={() => document.getElementById("avatar")?.click()}
+          onClick={focus}
         />
-        <span className="absolute bottom-0 right-0 inline-flex items-center justify-center w-10 h-10 bg-white rounded-full">
-          <i className="bx bx-camera text-2xl text-gray-500"></i>
+        <span
+          role="button"
+          className="absolute bottom-2 text-white flex items-center justify-center gap-1 right-1/2 transform translate-x-1/2 translate-y-1/2 backdrop-blur-md border rounded-md px-3 py-1"
+          onClick={focus}
+          title={t("upload")}
+          aria-label={t("upload")}
+        >
+          <i className="bx bx-xs bx-edit"></i>
+          {t("edit")}
         </span>
       </div>
       <input
@@ -71,7 +115,7 @@ export default function AvatarUpload({
         accept={accept}
         onChange={handleChange}
       />
-      {error && <p className="mt-2 text-xs text-red-500">{error}</p>}
+      {error && <p className="mt-5 text-sm text-red-500">{error}</p>}
     </div>
   );
 }
