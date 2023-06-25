46653c70f90b542d3b3965fc436eaab4b8503562
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-25 23:33:54 +0300

    feat(#53): add form texts for activation and deletion

--- src/app/[locale]/account/details/edit/components/AccountActivationForm.tsx
+++ src/app/[locale]/account/details/edit/components/AccountActivationForm.tsx
@@ -1,24 +1,32 @@
 "use client";
 
-import { useState } from "react";
+import { useTranslations } from "next-intl";
+import { useSelector } from "react-redux";
 import LineForm from "~/components/form/LineForm";
 import ToggleButton from "~/components/form/Toggle";
+import { RootState } from "~/store/store";
 
 export default function AccountActivationForm() {
-  const [isChecked, setIsChecked] = useState(false);
+  const account = useSelector(
+    (state: RootState) => state.account.currentAccount
+  );
+  const t = useTranslations("account.details.edit.activation");
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
+  const handleCheckboxChange = () => {};
 
   return (
     <LineForm className="bg-second p-4 rounded-md">
       <LineForm.Left>
-        <LineForm.Left.Title>Hesap Durumu</LineForm.Left.Title>
-        <LineForm.Left.Description></LineForm.Left.Description>
+        <LineForm.Left.Title>{t("title")}</LineForm.Left.Title>
+        <LineForm.Left.Description>
+          {t(account && account.isActive ? "active" : "passive")}
+        </LineForm.Left.Description>
       </LineForm.Left>
       <LineForm.Right>
-        <ToggleButton defaultChecked={false} variant="success"></ToggleButton>
+        <ToggleButton
+          defaultChecked={account && account.isActive ? true : false}
+          variant="success"
+        ></ToggleButton>
       </LineForm.Right>
     </LineForm>
   );
--- src/app/[locale]/account/details/edit/components/AccountDeletionForm.tsx
+++ src/app/[locale]/account/details/edit/components/AccountDeletionForm.tsx
@@ -1,22 +1,20 @@
 "use client";
 
-import { useState } from "react";
+import { useTranslations } from "next-intl";
 import LineForm from "~/components/form/LineForm";
 import ToggleButton from "~/components/form/Toggle";
 
 export default function AccountDeletionForm() {
-  const [isChecked, setIsChecked] = useState(false);
+  const t = useTranslations("account.details.edit.deletion");
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
+  const handleCheckboxChange = () => {};
 
   return (
     <LineForm className="bg-second p-4 rounded-md">
       <LineForm.Left>
-        <LineForm.Left.Title>Hesabımı Sil</LineForm.Left.Title>
+        <LineForm.Left.Title>{t("title")}</LineForm.Left.Title>
         <LineForm.Left.Description>
-          Yandaki kutucuğu işaretleyerek hesabınızı silebilirsiniz.
+          {t("description")}
         </LineForm.Left.Description>
       </LineForm.Left>
       <LineForm.Right>
