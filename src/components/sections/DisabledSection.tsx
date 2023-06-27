afc8d7cec7d529e3cdc49982d4d03a403f614ccc
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-27 19:17:35 +0300

    feat(#53): update the disable section responsive design

--- src/components/sections/DisabledSection.tsx
+++ src/components/sections/DisabledSection.tsx
@@ -27,6 +27,7 @@ type SizeType = {
   icon: string;
   title: string;
   description: string;
+  mobileMargin: string;
 };
 
 const Sizes: Record<Size, SizeType> = {
@@ -36,6 +37,7 @@ const Sizes: Record<Size, SizeType> = {
     icon: "bx-md",
     title: "text-sm",
     description: "text-xs",
+    mobileMargin: "pt-[150px] md:pt-[120px]",
   },
   md: {
     content: "p-4 gap-4",
@@ -43,6 +45,7 @@ const Sizes: Record<Size, SizeType> = {
     icon: "bx-2xl",
     title: "text-base",
     description: "text-sm",
+    mobileMargin: "pt-[250px] md:pt-[200px]",
   },
 };
 
@@ -51,25 +54,25 @@ const Variants: Record<Variant, Color> = {
     content: "bg-gray-200 dark:bg-gray-700",
     iconName: "bx-lock-alt",
     icon: "text-gray-500",
-    overlay: "border border-gray-300 dark:border-gray-600",
+    overlay: "border-gray-300 dark:border-gray-600",
   },
   error: {
     content: "bg-red-200 dark:bg-red-900",
     iconName: "bx-error",
     icon: "text-red-700 dark:text-red-300",
-    overlay: "border border-red-500 dark:border-red-400",
+    overlay: "border-red-500 dark:border-red-400",
   },
   maintenance: {
     content: "bg-yellow-200 dark:bg-yellow-900",
     iconName: "bx-traffic-cone",
     icon: "text-yellow-600 dark:text-yellow-300",
-    overlay: "border border-yellow-500",
+    overlay: "border-yellow-500",
   },
   vip: {
     content: "bg-vip-200 dark:bg-vip-1200",
     iconName: "bxl-sketch",
     icon: "text-vip-400 dark:text-vip-500",
-    overlay: "border border-vip dark:border-vip-500",
+    overlay: "border-vip dark:border-vip-500",
   },
 };
 
@@ -87,7 +90,8 @@ export default function DisabledSection({
 }: React.PropsWithChildren<Props>) {
   return (
     <>
-      <div className="relative  w-full h-full text-center border-2 rounded-md">
+      <div className="relative  w-full h-full text-center border-2 rounded-md ">
+        <div className={`lg:hidden ${Sizes[size].mobileMargin}`}></div>
         <div
           className={`w-full ${blur ? "blur-3xs" : ""} ${
             brightness ? "brightness-80" : ""
@@ -97,13 +101,13 @@ export default function DisabledSection({
         </div>
         <span
           className={`
-      absolute top-0 left-0 w-full flex flex-col items-center justify-center h-full bg-opacity-20 dark:bg-opacity-20 
+      absolute top-0 left-0 w-full flex flex-col items-center lg:justify-center h-full bg-opacity-20 dark:bg-opacity-20 p-4
       ${rounded ? "rounded-md" : ""}
         ${Variants[variant].content}
       `}
         >
           <div
-            className={` flex items-center justify-center rounded-md bg-second ${Variants[variant].overlay} ${Sizes[size].content}`}
+            className={` flex flex-col lg:flex-row border items-center justify-center rounded-md bg-second ${Variants[variant].overlay} ${Sizes[size].content}`}
           >
             <i
               className={`bx ${Sizes[size].icon} ${
@@ -111,7 +115,7 @@ export default function DisabledSection({
               } ${Variants[variant].icon}`}
             ></i>
             <div
-              className={`flex flex-col border-l text-gray-400 dark:border-l-gray-600 ${Sizes[size].stick}`}
+              className={`flex flex-col lg:border-l text-gray-400 dark:border-l-gray-600 ${Sizes[size].stick}`}
             >
               <div
                 className={`${Sizes[size].title} text-left font-semibold text-gray-800 dark:text-gray-200`}
