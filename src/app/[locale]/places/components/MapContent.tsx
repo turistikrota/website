d349e24f8ff3794ad7b7220737863591ee1b48dc
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-30 18:17:33 +0300

    map: update the icon

--- src/app/[locale]/places/components/MapContent.tsx
+++ src/app/[locale]/places/components/MapContent.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { type LatLngTuple } from "leaflet";
+import Leaflet, { type LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import { Marker, Popup } from "react-leaflet";
 import MapDefaultConfig from "~/components/map/MapDefaultConfig";
 
 const DynamicMap = dynamic(() => import("~/components/map/MapDynamic"), {
@@ -12,10 +14,23 @@ const DynamicMap = dynamic(() => import("~/components/map/MapDynamic"), {
 const position: LatLngTuple = [41.0082, 28.9784];
 
 export default function MapContent() {
+  useEffect(() => {
+    Leaflet.Icon.Default.mergeOptions({
+      iconRetinaUrl: "/images/marker-icon.png",
+      iconUrl: "/images/marker-icon.png",
+      shadowUrl: "",
+    });
+  }, []);
+
   return (
     <>
       <DynamicMap position={position}>
         <MapDefaultConfig />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
       </DynamicMap>
     </>
   );
--- src/components/map/child/AnimationClick.tsx
+++ src/components/map/child/AnimationClick.tsx
@@ -7,5 +7,5 @@ export default function AnimationClick() {
     });
   });
 
-  return null;
+  return <> </>;
 }
