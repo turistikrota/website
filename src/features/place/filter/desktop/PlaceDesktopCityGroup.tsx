3d0152ad975004ee0753c9ee21f867d82d7b84ef
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-02 20:56:30 +0300

    feat(#76): complete the city filter with i18n and clearable

--- src/features/place/filter/desktop/PlaceDesktopCityGroup.tsx
+++ src/features/place/filter/desktop/PlaceDesktopCityGroup.tsx
@@ -1,12 +1,30 @@
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
+import { City, findCityByCoordinates } from "~/static/location/cities";
+import { usePlaceFilter } from "../../place.filter";
 import PlaceFilterCityGroup from "../shared/PlaceFilterCityGroup";
 import PlaceDesktopHead from "./PlaceDesktopHead";
 
 export default function PlaceDesktopCityGroup() {
+  const [city, setCity] = useState<City | null>(null);
+  const t = useTranslations("place.filter.components.city-select");
+  const { query, push } = usePlaceFilter();
+
+  useEffect(() => {
+    if (query.filter.coordinates) {
+      setCity(findCityByCoordinates(query.filter.coordinates));
+    }
+  }, [query]);
+
+  const clearCity = () => {
+    query.filter.coordinates = undefined;
+    push(query);
+  };
   return (
     <div className="border-b p-4">
       <PlaceDesktopHead>
-        <PlaceDesktopHead.Title>City</PlaceDesktopHead.Title>
-        <PlaceDesktopHead.Clear />
+        <PlaceDesktopHead.Title>{t("text")}</PlaceDesktopHead.Title>
+        {!!city && <PlaceDesktopHead.Clear onClear={clearCity} />}
       </PlaceDesktopHead>
       <PlaceFilterCityGroup className="max-h-60 mt-2" />
     </div>
