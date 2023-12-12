f3b6483d0272e547df71c0e81c28f8a3e23ea114
9ssi7 <info@ssibrahimbas.com>
2023-12-12 12:02:12 +0300

    feature/home-filter: add onclick handler

--- src/features/home/CategoryFilterSection.tsx
+++ src/features/home/CategoryFilterSection.tsx
@@ -4,8 +4,9 @@ import Button from '@turistikrota/ui/button'
 import Input from '@turistikrota/ui/form/input'
 import Select from '@turistikrota/ui/form/select'
 import { useIsDesktop } from '@turistikrota/ui/hooks/dom'
-import { useTranslations } from 'next-intl'
+import { useLocale, useTranslations } from 'next-intl'
 import { FC, useState } from 'react'
+import { getStaticRoute } from '~/static/page'
 
 enum Category {
   Listing = 'listing',
@@ -14,11 +15,19 @@ enum Category {
 
 const CategoryFilterSection: FC = () => {
   const t = useTranslations('home.category-filter')
+  const locale = useLocale()
   const isDesktop = useIsDesktop()
   const [category, setCategory] = useState<Category>(Category.Listing)
   const [startDate, setStartDate] = useState<string | undefined>(undefined)
   const [endDate, setEndDate] = useState<string | undefined>(undefined)
   const [word, setWord] = useState<string>('')
+
+  const onClick = () => {
+    if (category === Category.Place) {
+      window.open(getStaticRoute(locale).places + `${word ? `?q=${word}` : ''}`, '_blank')
+    }
+    // listing not yet implemented
+  }
   return (
     <div className='z-10 mb-10 flex w-full flex-col gap-4 rounded-md bg-second p-4 backdrop-blur-xs md:mb-44 md:max-w-4xl md:flex-row'>
       <div className='grid grow grid-cols-12 gap-4'>
@@ -28,6 +37,9 @@ const CategoryFilterSection: FC = () => {
             name='category'
             onChange={(e) => {
               setCategory(e.target.value as Category)
+              setStartDate(undefined)
+              setEndDate(undefined)
+              setWord('')
             }}
           >
             <option value={Category.Listing}>{t(Category.Listing)}</option>
@@ -73,7 +85,12 @@ const CategoryFilterSection: FC = () => {
         )}
       </div>
       <div className='flex min-w-fit items-center'>
-        <Button variant='primary' className='flex items-center justify-center gap-2' block={!isDesktop}>
+        <Button
+          variant='primary'
+          className='flex items-center justify-center gap-2'
+          block={!isDesktop}
+          onClick={onClick}
+        >
           <i className='bx bx-search-alt-2 text-lg'></i>
           {t(category === Category.Listing ? 'search' : 'explore')}
         </Button>
