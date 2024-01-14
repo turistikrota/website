add76234f9319b34fa8938c6bad52a99e4987939
9ssi7 <info@ssibrahimbas.com>
2024-01-14 15:13:40 +0300

    feature/contact-info: update

--- src/app/[locale]/contact/components/ContactInfoSection.tsx
+++ src/app/[locale]/contact/components/ContactInfoSection.tsx
@@ -10,12 +10,12 @@ const ContactInfoSection: FC = () => {
       <LandingSection.Head title={t('title')} subtitle={t('subtitle')} />
       <div className='mt-14 grid grid-cols-2 gap-2 pb-2'>
         <KeyValue label={t('tradeName')} value='Turistik Rota' />
+        <KeyValue label={t('companyName')} value='Sami Salih İbrahimbaş' />
         <KeyValue
           label={t('address')}
           value='Çayiçi mahallesi Sönmez caddesi no:75 iç kapı no:1 Sapanca/Sakarya, Türkiye'
         />
         <KeyValue label={t('kepAddress')} value='samisalih.ibrahimbas@hs01.kep.tr' />
-        <KeyValue label={t('taxOffice')} value='Sapanca' />
         <KeyValue label={t('supportEmail')} value='support@turistikrota.com' />
         <KeyValue label={t('techEmail')} value='tech@turistikrota.com' />
       </div>
